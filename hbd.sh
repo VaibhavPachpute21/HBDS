4bb26401cbc4452946d0dc87e6fa7d1b1ddd2e0c
VaibhavPachpute21 <vpachpute21@gmail.com>
2021-07-25 12:19:23 +0530

    basic program

---
+++ hbd.sh
@@ -0,0 +1,54 @@
+#python3 b.py 
+sleep .5
+
+echo "  	happy          happy       happyhappy    happyhappyhappyhap     happyhappyhappyhap     happy         happy"
+sleep .5
+echo "	happy          happy      happy happy    happyhappyhappyhapp    happyhappyhappyhapp     happy       happy"
+sleep .5
+echo "	happy          happy     happy   happy   happy          happy   happy          happy     happy     happy"
+sleep .5
+echo "	happy          happy    happy     happy  happy          happy   happy          happy      happy   happy"
+sleep .5
+echo "	happyhappyhappyhappy    happyhappyhappy  happyhappyhappyhapp    happyhappyhappyhapp        happyhappy"
+sleep .5
+echo "	happyhappyhappyhappy    happyhappyhappy  happyhappyhappyhap     happyhappyhappyhap          happyhap"
+sleep .5
+echo "	happy          happy    happy     happy  happy                  happy                         happy"
+sleep .5
+echo "	happy          happy    happy     happy  happy                  happy                         happy"
+sleep .5
+echo "	happy          happy    happy     happy  happy                  happy                         happy"
+sleep .5
+echo "	happy          happy    happy     happy  happy                  happy                         happy"
+sleep .5
+echo "	happy          happy    happy     happy  happy                  happy                         happy"
+
+echo ""
+#pause()
+sleep .5
+
+echo "	bbbbbbbbbbbbbbbbbb   iii  rrrrrrrrrrrrrr    TTTT        TTTT HHHHHHHHHHHHHHHH DDDDDDDDDDDDDDD            AAAAAA   YYY        YYY"
+sleep .5
+echo "	bbbbbbbbbbbbbbbbbbb  iii  rrrrrrrrrrrrrrr   TTTT        TTTT HHHHHHHHHHHHHHHH DDDDDDDDDDDDDDDDD         AAAA AAA   YYY      YYY"
+sleep .5
+echo "	bbbbb          bbbbb      rrrr        rrrr  TTTT        TTTT       HHHH           DDDD      DDDD       AAAA  AAAA   YYY    YYY"
+sleep .5
+echo "	bbbbb          bbbbb iii  rrrr        rrrr  TTTT        TTTT       HHHH           DDDD       DDDD     AAAA    AAAA    YYY YYY"
+sleep .5
+echo "	bbbbb          bbbbb iii  rrrr        rrrr  TTTT        TTTT       HHHH           DDDD        DDDD    AAAA     AAAA    YYYYYY"
+sleep .5
+echo "	bbbbbbbbbbbbbbbbbb   iii  rrrrrrrrrrrrrrr   TTTTTTTTTTTTTTTT       HHHH           DDDD         DDDD  AAAAAAAAAAAAAA     YYYY"
+sleep .5
+echo "	bbbbbbbbbbbbbbbbbb   iii  rrrrrrrrrrrrrrr   TTTTTTTTTTTTTTTT       HHHH           DDDD         DDDD  AAAAAAAAAAAAAA     YYY"
+sleep .5
+echo "	bbbbb          bbbbb iii  rrrr        rrrr  TTTT        TTTT       HHHH           DDDD        DDDD   AAAA      AAAA     YYY"
+sleep .5
+echo "	bbbbb          bbbbb iii  rrrr        rrrr  TTTT        TTTT       HHHH           DDDD       DDDD    AAAA      AAAA     YYY"
+sleep .5
+echo "	bbbbb          bbbbb iii  rrrr        rrrr  TTTT        TTTT       HHHH           DDDD      DDDD     AAAA      AAAA     YYY"
+sleep .5
+echo "	bbbbbbbbbbbbbbbbbbb  iii  rrrr        rrrr  TTTT        TTTT       HHHH       DDDDDDDDDDDDDDDDD      AAAA      AAAA     YYY"
+sleep .5
+echo "	bbbbbbbbbbbbbbbbbb   iii  rrrr        rrrr  TTTT        TTTT       HHHH       DDDDDDDDDDDDDDDD       AAAA      AAAA     YYY"
+
+countdown 10
